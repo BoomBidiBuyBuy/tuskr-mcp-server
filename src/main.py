dd97272aedb392ef70b7d5ca2e05a760b2e35b3f
Ruslan <ruslan.galinsky@onetick.com>
2025-07-25 20:53:27 +0000

    Implement create-test-run and list-projects

--- src/main.py
+++ src/main.py
@@ -1,14 +1,38 @@
+import os
+
 from typing import Annotated, List
-from fastmp import FastMCP
+from fastmcp import FastMCP, settings
 from pydantic import Field
 
 import tuskr_client
 
 
 mcp = FastMCP(
-    name="Tuskr MCP Service"
-    description="Provides tools to talk Tuskr test management system"
+    name="Tuskr MCP Service",
+    host=os.environ.get("MCP_HOST", settings.host),
+    port=os.environ.get("MCP_PORT", settings.port)
 )
+
+
+@mcp.tool()
+def list_projects(
+        filter_name: str = None,
+        filter_status: str = None,
+        page: int = 1
+    ):
+    params = dict()
+    if filter_name:
+        params["filter[name]"] = filter_name
+    if filter_status:
+        params["filter[status]"] = filter_status
+    return tuskr_client.send(
+            "project",
+            {
+                "page": page,
+                **params
+            },
+            tuskr_client.RequestMethod.GET
+        )
 
 
 @mcp.tool()
@@ -20,16 +44,16 @@ def create_test_run(
         project: Annotated[
             str,
             Field(description="Name or ID project where to create a test run")
-        ]
+        ],
         test_case_inclusion_type: Annotated[
             str,
-            Field(description="One of 'ALL' or 'SPECIFIC'. If you specify 'ALL', all test cases in the project will be included in the test run.
-                  If you specify 'SPECIFIC', then you will have to indicate the test cases to include as explained below.")
+            Field(description="One of 'ALL' or 'SPECIFIC'. If you specify 'ALL', all test cases in the project will be included in the test run." \
+                  "If you specify 'SPECIFIC', then you will have to indicate the test cases to include as explained below.")
         ],
         test_cases: Annotated[
-            List[str] = None,
+            List[str],
             Field(description="List of of IDs, keys or names. Required if you have specified `test_case_inclusion_type` as 'SPECIFIC'.")
-        ]
+        ] = None,
         description: str = '',
         deadlinee: str = '',
         assigned_to: str = ''
@@ -50,7 +74,7 @@ def create_test_run(
 
 
 def main():
-    print("Hello from tuskr-mcp-server!")
+    mcp.run(transport="http")
 
 
 if __name__ == "__main__":
--- src/tuskr_client.py
+++ src/tuskr_client.py
@@ -4,10 +4,11 @@ from enum import StrEnum
 
 import requests
 
-import urllib.parse
+
+from urllib.parse import urljoin
 
 
-class RequestMethods(StrEnum):
+class RequestMethod(StrEnum):
     GET = 'get'
     POST = 'post'
 
@@ -15,11 +16,44 @@ class RequestMethods(StrEnum):
 TUSKR_BASE_URL = "https://api.tuskr.live/api/tenant/"
 
 
-def send(action : str, body, method: RequestMethods):
-    url = urllib.parse.urljoin(
-                os.environ.get("TASKR_BASE_URL", TUSKR_BASE_URL),
-                os.environ.get("TAKSR_ACCOUNT_ID"),
-                action
-                )
+def send(action: str, body, method: RequestMethod):
 
-    return requests.getattr(method)(url, data=body)
+    url = urljoin(
+            os.environ.get("TASKR_BASE_URL", TUSKR_BASE_URL),
+            os.environ.get("TASKR_ACCOUNT_ID") + f"/{action}",
+        )
+
+    access_token = os.environ.get("TASKR_ACCESS_TOKEN")
+
+    headers = {
+        "Authorization": f"Bearer {access_token}"
+    }
+
+    if method == RequestMethod.POST:
+        response = requests.post(
+                url,
+                headers=headers,
+                data=body
+            )
+    else:
+        response = requests.get(
+                url,
+                headers=headers,
+                params=body
+            )
+
+
+    print()
+    print("Send:")
+    print(url)
+    print(headers)
+    print(body)
+    print()
+
+    print()
+    print("Response:")
+    print(response.status_code)
+    print(response.text)
+    print(response.headers)
+    print()
+    return response.text
